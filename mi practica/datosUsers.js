4a3fd1994d86bbe84f9f99bb44970aa382790fe9
Washington <117743091+washito0407@users.noreply.github.com>
2023-11-12 07:29:12 -0800

    Add files via upload

---
+++ mi practica/datosUsers.js
@@ -0,0 +1,22 @@
+const obtenerUsaurios = async () => {
+    try {
+        let response = await fetch(url);
+        if (!response.ok) {
+        throw new Error("Ourrio un Error al realizar la petición");
+        }
+        let data = await response.json();
+        pintarUsuarios(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const pintarUsuarios = (data) => 
+{
+    let body = "";
+    for (let i = 0; i < data.length; i++) 
+    {
+        body += `<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`;
+    }
+    document.getElementById("data").innerHTML = body;
+};
